5e07ac38091bd302fc5d07e6f34ee7fe6afb456b
richard morris <richard@corkmate.com>
2023-12-07 13:40:22 +0000

    fix binding for generate_random_tree

--- core/generate_random_tree_bindings.cpp
+++ core/generate_random_tree_bindings.cpp
@@ -5,5 +5,5 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(mylib, m) {
     m.doc() = "Python bindings for the IQ-TREE function to generate a random tree";
-    m.def("generate_random_tree", &generateRandomTree, "Generate a random tree");
+    m.def("generate_random_tree", &generate_random_tree, "Generate a random tree");
 }
