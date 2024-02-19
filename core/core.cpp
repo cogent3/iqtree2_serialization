02e246683fd9776649e9fc445b3b10ba5febe4c3
= <=>
2024-02-20 09:55:39 +1100

    renamed version_binding to core to init the python module in C++

--- core/core.cpp
+++ core/core.cpp
@@ -4,31 +4,6 @@
 #include <pybind11/pybind11.h>
 #include "iqtree_config.h"
 
-static struct PyModuleDef iqtree2coremodule = {
-    PyModuleDef_HEAD_INIT,
-    "core",   /* name of module */
-    NULL,            /* module documentation, may be NULL */
-    -1,              /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
-    NULL, NULL, NULL, NULL, NULL
-};
-
-PyMODINIT_FUNC PyInit_core(void) {
-    std::cout << "Entering PyInit_core\n"; // Debug message
-    PyObject* m;
-
-    m = PyModule_Create(&iqtree2coremodule);
-    if (m == NULL)
-        return NULL;
-
-    std::cout << "Created module\n"; // Debug message
-
-    /* Add other module level functions, constants, and variables here */
-
-    std::cout << "Exiting PyInit_core\n"; // Debug message
-    return m;
-}
-
-
 // Assuming iqtree_VERSION_MAJOR, iqtree_VERSION_MINOR, and iqtree_VERSION_PATCH are defined somewhere globally
 const int iqtree_version_major = iqtree_VERSION_MAJOR;
 const int iqtree_version_minor = iqtree_VERSION_MINOR;
@@ -53,7 +28,7 @@ std::string getIqTreeVersion() {
     return version; // Return the std::string object
 }
 
-PYBIND11_MODULE(version_bindings, m) {
+PYBIND11_MODULE(core, m) {
     m.doc() = "Python bindings for the IQ-TREE version information";
 
     m.def("get_iq_tree_version", &getIqTreeVersion, "Get the IQ-TREE version string.");
